bb71f468c881d5032be8ec395ebfb4f79c37a265
Ol3k1410 <olekkocowicz566@gmail.com>
2026-04-10 16:49:22 +0200

    Projekt_struktury_danych.cpp

--- Projekt_struktury_danych.cpp
+++ Projekt_struktury_danych.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <vector>
 #include <algorithm>
 #include <numeric>
@@ -8,473 +8,441 @@
 using namespace std;
 using namespace chrono;
 
-//////////////////////////////////////////////////
-// TABLICA DYNAMICZNA
-//////////////////////////////////////////////////
-
 class DynamicArray {
-
 private:
-
     int* arr;
     int size;
     int capacity;
 
     void resize() {
-
         capacity *= 2;
-
         int* newArr = new int[capacity];
 
-        for(int i=0;i<size;i++)
+        for (int i = 0; i < size; i++)
             newArr[i] = arr[i];
 
         delete[] arr;
-
         arr = newArr;
     }
 
 public:
-
     DynamicArray(int cap = 4) {
-
         capacity = cap;
         size = 0;
-
         arr = new int[capacity];
     }
 
-    // COPY CONSTRUCTOR
-
     DynamicArray(const DynamicArray& other) {
-
         size = other.size;
         capacity = other.capacity;
 
         arr = new int[capacity];
 
-        for(int i=0;i<size;i++)
+        for (int i = 0; i < size; i++)
             arr[i] = other.arr[i];
     }
 
+    DynamicArray& operator=(const DynamicArray& other) {
+        if (this == &other) return *this;
+
+        delete[] arr;
+
+        size = other.size;
+        capacity = other.capacity;
+        arr = new int[capacity];
+
+        for (int i = 0; i < size; i++)
+            arr[i] = other.arr[i];
+
+        return *this;
+    }
+
+    ~DynamicArray() {
+        delete[] arr;
+    }
+
     void addFront(int val) {
+        if (size == capacity) resize();
 
-        if(size == capacity) resize();
-
-        for(int i=size;i>0;i--)
-            arr[i] = arr[i-1];
+        for (int i = size; i > 0; i--)
+            arr[i] = arr[i - 1];
 
         arr[0] = val;
-
         size++;
     }
 
     void addBack(int val) {
-
-        if(size == capacity) resize();
-
+        if (size == capacity) resize();
         arr[size++] = val;
     }
 
-    void addAt(int index,int val) {
+    void addAt(int index, int val) {
+        if (index < 0 || index > size) return;
 
-        if(size == capacity) resize();
+        if (size == capacity) resize();
 
-        for(int i=size;i>index;i--)
-            arr[i] = arr[i-1];
+        for (int i = size; i > index; i--)
+            arr[i] = arr[i - 1];
 
         arr[index] = val;
-
         size++;
     }
 
     void removeFront() {
+        if (size == 0) return;
 
-        for(int i=0;i<size-1;i++)
-            arr[i] = arr[i+1];
+        for (int i = 0; i < size - 1; i++)
+            arr[i] = arr[i + 1];
 
         size--;
     }
 
     void removeBack() {
-
+        if (size == 0) return;
         size--;
     }
 
     void removeAt(int index) {
+        if (index < 0 || index >= size) return;
 
-        for(int i=index;i<size-1;i++)
-            arr[i] = arr[i+1];
+        for (int i = index; i < size - 1; i++)
+            arr[i] = arr[i + 1];
 
         size--;
     }
 
     int search(int val) {
-
-        for(int i=0;i<size;i++)
-            if(arr[i] == val)
+        for (int i = 0; i < size; i++)
+            if (arr[i] == val)
                 return i;
 
         return -1;
     }
-
 };
 
-//////////////////////////////////////////////////
-// LISTA WIĄZANA
-//////////////////////////////////////////////////
-
 struct Node {
-
     int data;
     Node* next;
-
 };
 
 class LinkedList {
-
 private:
-
     Node* head;
+    int size;
 
 public:
-
     LinkedList() {
-
         head = nullptr;
+        size = 0;
     }
 
-    // COPY CONSTRUCTOR
-
     LinkedList(const LinkedList& other) {
-
         head = nullptr;
+        size = 0;
 
         Node* temp = other.head;
         Node* tail = nullptr;
 
-        while(temp) {
+        while (temp) {
+            Node* newNode = new Node{temp->data, nullptr};
 
-            Node* newNode = new Node{temp->data,nullptr};
-
-            if(!head) {
-
+            if (!head) {
                 head = newNode;
                 tail = newNode;
-
             } else {
-
                 tail->next = newNode;
                 tail = newNode;
-
             }
 
             temp = temp->next;
+            size++;
+        }
+    }
+
+    LinkedList& operator=(const LinkedList& other) {
+        if (this == &other) return *this;
+
+        while (head) {
+            Node* tmp = head;
+            head = head->next;
+            delete tmp;
+        }
+
+        head = nullptr;
+        size = 0;
+
+        Node* temp = other.head;
+        Node* tail = nullptr;
+
+        while (temp) {
+            Node* newNode = new Node{temp->data, nullptr};
+
+            if (!head) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail->next = newNode;
+                tail = newNode;
+            }
+
+            temp = temp->next;
+            size++;
+        }
+
+        return *this;
+    }
+
+    ~LinkedList() {
+        while (head) {
+            Node* tmp = head;
+            head = head->next;
+            delete tmp;
         }
     }
 
     void addFront(int val) {
-
-        Node* n = new Node{val,head};
-
+        Node* n = new Node{val, head};
         head = n;
+        size++;
     }
 
     void addBack(int val) {
+        Node* n = new Node{val, nullptr};
 
-        Node* n = new Node{val,nullptr};
-
-        if(!head) {
-
+        if (!head) {
             head = n;
-            return;
+        } else {
+            Node* t = head;
+            while (t->next)
+                t = t->next;
+            t->next = n;
         }
 
-        Node* t = head;
-
-        while(t->next)
-            t = t->next;
-
-        t->next = n;
+        size++;
     }
 
-    void addAt(int index,int val) {
+    void addAt(int index, int val) {
+        if (index < 0 || index > size) return;
 
-        if(index == 0) {
-
+        if (index == 0) {
             addFront(val);
             return;
         }
 
         Node* t = head;
-
-        for(int i=0;i<index-1;i++)
+        for (int i = 0; i < index - 1; i++)
             t = t->next;
 
-        Node* n = new Node{val,t->next};
-
+        Node* n = new Node{val, t->next};
         t->next = n;
+
+        size++;
     }
 
     void removeFront() {
+        if (!head) return;
 
         Node* t = head;
-
         head = head->next;
-
         delete t;
+
+        size--;
     }
 
     void removeBack() {
+        if (!head) return;
 
-        if(!head->next) {
-
+        if (!head->next) {
             delete head;
             head = nullptr;
-
+            size--;
             return;
         }
 
         Node* t = head;
 
-        while(t->next->next)
+        while (t->next->next)
             t = t->next;
 
         delete t->next;
-
         t->next = nullptr;
+
+        size--;
     }
 
     void removeAt(int index) {
+        if (index < 0 || index >= size) return;
 
-        if(index == 0) {
-
+        if (index == 0) {
             removeFront();
             return;
         }
 
         Node* t = head;
 
-        for(int i=0;i<index-1;i++)
+        for (int i = 0; i < index - 1; i++)
             t = t->next;
 
         Node* del = t->next;
-
         t->next = del->next;
 
         delete del;
+
+        size--;
     }
 
     int search(int val) {
-
         Node* t = head;
-
         int i = 0;
 
-        while(t) {
-
-            if(t->data == val)
+        while (t) {
+            if (t->data == val)
                 return i;
 
             t = t->next;
-
             i++;
         }
 
         return -1;
     }
-
 };
 
-//////////////////////////////////////////////////
-// USUWANIE SKRAJNYCH WARTOŚCI
-//////////////////////////////////////////////////
-
 double averageTrim(vector<long long> times) {
-
-    sort(times.begin(),times.end());
+    sort(times.begin(), times.end());
 
     int trim = times.size() * 0.05;
 
     vector<long long> trimmed(
-        times.begin()+trim,
-        times.end()-trim
+        times.begin() + trim,
+        times.end() - trim
     );
 
-    double sum = accumulate(trimmed.begin(),trimmed.end(),0.0);
-
+    double sum = accumulate(trimmed.begin(), trimmed.end(), 0.0);
     return sum / trimmed.size();
 }
 
-//////////////////////////////////////////////////
-// POMIAR CZASU
-//////////////////////////////////////////////////
-
 template<typename Func>
 double measure(Func f) {
-
     vector<long long> times;
 
-    for(int i=0;i<100;i++) {
-
+    for (int i = 0; i < 100; i++) {
         auto start = high_resolution_clock::now();
-
         f();
-
         auto end = high_resolution_clock::now();
 
         times.push_back(
-            duration_cast<nanoseconds>(end-start).count()
+            duration_cast<nanoseconds>(end - start).count()
         );
     }
 
     return averageTrim(times);
 }
 
-//////////////////////////////////////////////////
-// MAIN
-//////////////////////////////////////////////////
-
 int main() {
-
-    vector<int> sizes = {1000,5000,10000,50000,100000};
+    vector<int> sizes = {1000, 5000, 10000, 50000, 100000};
 
     ofstream file("results.csv");
+    file << "Structure,Operation,Size,Time(ns)\n";
 
-    file<<"Structure,Operation,Size,Time(ns)\n";
-
-    for(int n : sizes) {
+    for (int n : sizes) {
 
         DynamicArray arr;
         LinkedList list;
 
-        for(int i=0;i<n;i++) {
-
+        for (int i = 0; i < n; i++) {
             arr.addBack(i);
             list.addBack(i);
         }
 
         double t;
 
-        // TABLICA
-
-        t = measure([&](){
-
+        t = measure([&]() {
             DynamicArray temp = arr;
             temp.addFront(1);
         });
+        file << "DynamicArray,AddFront," << n << "," << t << "\n";
 
-        file<<"DynamicArray,AddFront,"<<n<<","<<t<<"\n";
-
-        t = measure([&](){
-
+        t = measure([&]() {
             DynamicArray temp = arr;
             temp.addBack(1);
         });
+        file << "DynamicArray,AddBack," << n << "," << t << "\n";
 
-        file<<"DynamicArray,AddBack,"<<n<<","<<t<<"\n";
-
-        t = measure([&](){
-
+        t = measure([&]() {
             DynamicArray temp = arr;
-            temp.addAt(n/2,1);
+            temp.addAt(n / 2, 1);
         });
+        file << "DynamicArray,AddMiddle," << n << "," << t << "\n";
 
-        file<<"DynamicArray,AddMiddle,"<<n<<","<<t<<"\n";
-
-        t = measure([&](){
-
+        t = measure([&]() {
             DynamicArray temp = arr;
             temp.removeFront();
         });
+        file << "DynamicArray,RemoveFront," << n << "," << t << "\n";
 
-        file<<"DynamicArray,RemoveFront,"<<n<<","<<t<<"\n";
-
-        t = measure([&](){
-
+        t = measure([&]() {
             DynamicArray temp = arr;
             temp.removeBack();
         });
+        file << "DynamicArray,RemoveBack," << n << "," << t << "\n";
 
-        file<<"DynamicArray,RemoveBack,"<<n<<","<<t<<"\n";
-
-        t = measure([&](){
-
+        t = measure([&]() {
             DynamicArray temp = arr;
-            temp.removeAt(n/2);
+            temp.removeAt(n / 2);
         });
+        file << "DynamicArray,RemoveMiddle," << n << "," << t << "\n";
 
-        file<<"DynamicArray,RemoveMiddle,"<<n<<","<<t<<"\n";
-
-        t = measure([&](){
-
+        t = measure([&]() {
             DynamicArray temp = arr;
-            temp.search(n/2);
+            temp.search(n / 2);
         });
+        file << "DynamicArray,Search," << n << "," << t << "\n";
 
-        file<<"DynamicArray,Search,"<<n<<","<<t<<"\n";
-
-        // LISTA
-
-        t = measure([&](){
-
+        t = measure([&]() {
             LinkedList temp = list;
             temp.addFront(1);
         });
+        file << "LinkedList,AddFront," << n << "," << t << "\n";
 
-        file<<"LinkedList,AddFront,"<<n<<","<<t<<"\n";
-
-        t = measure([&](){
-
+        t = measure([&]() {
             LinkedList temp = list;
             temp.addBack(1);
         });
+        file << "LinkedList,AddBack," << n << "," << t << "\n";
 
-        file<<"LinkedList,AddBack,"<<n<<","<<t<<"\n";
-
-        t = measure([&](){
-
+        t = measure([&]() {
             LinkedList temp = list;
-            temp.addAt(n/2,1);
+            temp.addAt(n / 2, 1);
         });
+        file << "LinkedList,AddMiddle," << n << "," << t << "\n";
 
-        file<<"LinkedList,AddMiddle,"<<n<<","<<t<<"\n";
-
-        t = measure([&](){
-
+        t = measure([&]() {
             LinkedList temp = list;
             temp.removeFront();
         });
+        file << "LinkedList,RemoveFront," << n << "," << t << "\n";
 
-        file<<"LinkedList,RemoveFront,"<<n<<","<<t<<"\n";
-
-        t = measure([&](){
-
+        t = measure([&]() {
             LinkedList temp = list;
             temp.removeBack();
         });
+        file << "LinkedList,RemoveBack," << n << "," << t << "\n";
 
-        file<<"LinkedList,RemoveBack,"<<n<<","<<t<<"\n";
-
-        t = measure([&](){
-
+        t = measure([&]() {
             LinkedList temp = list;
-            temp.removeAt(n/2);
+            temp.removeAt(n / 2);
         });
+        file << "LinkedList,RemoveMiddle," << n << "," << t << "\n";
 
-        file<<"LinkedList,RemoveMiddle,"<<n<<","<<t<<"\n";
-
-        t = measure([&](){
-
+        t = measure([&]() {
             LinkedList temp = list;
-            temp.search(n/2);
+            temp.search(n / 2);
         });
-
-        file<<"LinkedList,Search,"<<n<<","<<t<<"\n";
+        file << "LinkedList,Search," << n << "," << t << "\n";
     }
 
     file.close();
 
-    cout<<"Pomiar zakonczony.\n";
-    cout<<"Wyniki zapisane w results.csv\n";
+    cout << "Pomiar zakonczony.\n";
+    cout << "Wyniki zapisane w results.csv\n";
 }
